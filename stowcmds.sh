1fef1a24ecc8974c32000cc6d755a39c7c8e3a68
Raymaekers Luca <luca@keyfried.com>
2023-02-16 00:36:23 +0100

    changed: (stowcmds) cleanup

--- stowcmds.sh
+++ stowcmds.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
-d="$1"
+mkdir "$HOME/bin"
+stow -d bin/ -t "$d/bin" common dmscripts extra
 
-mkdir "$d/bin"
-stow -d bin/ -t "$d/bin" common dmscripts extra zshrc
-
-mkdir "$d/.config"
-stow -d config/ -t "$d/.config" essentials common extra theme X
-
-stow -d config/ -t "$d/" zshrc
+mkdir "$HOME/.config"
+stow -d config/ -t "$HOME/.config" essentials common extra theme X
+stow -d config/ -t "$HOME/" zshrc
