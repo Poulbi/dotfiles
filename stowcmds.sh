9db9817c0afe939910d6085c9daf4385ab4caced
Raymaekers Luca <luca@keyfried.com>
2023-02-16 18:17:53 +0100

    added: (stowcmds) server configuration

--- stowcmds.sh
+++ stowcmds.sh
@@ -4,9 +4,15 @@ case "${MACH:=desktop}" in
 	'desktop')
 		mkdir "$HOME/bin"
 		stow -d bin/ -t "$HOME/bin" -R common dmscripts extra
-
 		mkdir "$HOME/.config"
 		stow -d config/ -t "$HOME/.config" -R essentials common extra theme X
+		stow -d config/ -t "$HOME/" -R zshrc
+		;;
+	"server")
+		mkdir -p "$HOME/bin"
+		stow -d bin/ -t "$HOME/bin" -R common
+		mkdir -p "$HOME/.config"
+		stow -d config/ -t "$HOME/.config" -R essentials common
 		stow -d config/ -t "$HOME/" -R zshrc
 		;;
 	*)
