f612b93446d86a382e5d1efb98b5a6da90c4e8d7
Raymaekers Luca <luca@keyfried.com>
2023-02-16 15:42:47 +0100

    stowcmds: added case statement for different machines

--- stowcmds.sh
+++ stowcmds.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
-mkdir "$HOME/bin"
-stow -d bin/ -t "$d/bin" common dmscripts extra
 
-mkdir "$HOME/.config"
-stow -d config/ -t "$HOME/.config" essentials common extra theme X
-stow -d config/ -t "$HOME/" zshrc
+case in "${MACH:=desktop}"
+	'desktop')
+		mkdir "$HOME/bin"
+		stow -d bin/ -t "$d/bin" -R common dmscripts extra
+
+		mkdir "$HOME/.config"
+		stow -d config/ -t "$HOME/.config" -R essentials common extra theme X
+		stow -d config/ -t "$HOME/" -R zshrc
+		;;
+esac
