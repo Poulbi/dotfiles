8ec93668fb0e6e18b48b173252582c65be8e8898
Raymaekers Luca <luca@keyfried.com>
2023-02-18 18:06:27 +0100

    changed: (nvim) telekasten to file type plugin

--- config/essentials/nvim/ftplugin/telekasten.lua
+++ config/essentials/nvim/ftplugin/telekasten.lua
@@ -1,3 +1,7 @@
+vim.keymap.set("i", "(", "()<Left>")
+vim.keymap.set("i", "[", "[]<Left>")
+
+-- syntax highlighting
 vim.cmd("hi tklink ctermfg=72 guifg=#81a1c1 cterm=bold,underline gui=bold,underline")
 vim.cmd("hi tkBrackets ctermfg=gray guifg=gray")
 vim.cmd("hi tkHighlight ctermbg=yellow ctermfg=red cterm=bold guibg=#ebcb8b guifg=black gui=bold")
