cad42fb9a1f0842704aa0c6c8b155d070ab80f77
Raymaekers Luca <luca@keyfried.com>
2023-02-19 14:22:52 +0100

    added: defaults for TERMINAL and BROWSER

--- config/essentials/zsh/variables.zsh
+++ config/essentials/zsh/variables.zsh
@@ -4,6 +4,8 @@ export ZOT="${HOME}/zot"
 
 export EDITOR="nvim"
 export VISUAL="nvim"
+export TERMINAL="alacritty"
+export BROWSER="firefox"
 
 # colored GCC warnings and errors
 export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
