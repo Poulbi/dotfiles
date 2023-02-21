66bbc651baec8bb25c5561fa531456d9ec21d261
Raymaekers Luca <luca@keyfried.com>
2023-02-21 21:43:56 +0100

    added: (nvim) java lsp

--- config/essentials/nvim/lua/tlast/cmp/setup.lua
+++ config/essentials/nvim/lua/tlast/cmp/setup.lua
@@ -18,11 +18,12 @@ cmp.setup({
 	}),
 	sources = cmp.config.sources({
 		{ name = 'nvim_lua' },
-		{ name = 'nvim_lsp', keyword_length = 4 },
+		{ name = 'nvim_lsp'},
 		{ name = 'path' },
 		{ name = 'luasnip' },
 		{ name = 'buffer', keyword_length = 4 },
 	}),
+	preselect = cmp.PreselectMode.None,
 })
 
 cmp.setup.filetype('gitcommit', {
