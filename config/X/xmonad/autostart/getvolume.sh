bc2b79c4429ddf7bc41066fa8e202b403be2f58a
Raymaekers Luca <luca@keyfried.com>
2023-02-27 13:16:02 +0100

    font should happen in xmobar

--- config/X/xmonad/autostart/getvolume.sh
+++ config/X/xmonad/autostart/getvolume.sh
@@ -5,16 +5,16 @@ do
     muted=$(pamixer --get-mute)
     if [[ "$muted" == "true" ]] 
     then
-      icon="<fn=1></fn>"
+      icon=""
     elif [ $string -gt 49 ]
     then
-      icon="<fn=1></fn>"
+      icon=""
     elif [ $string -eq 0 ]
     then
-      icon="<fn=1></fn>"
+      icon=""
     elif [ $string -lt 50 ]
     then
-      icon="<fn=1></fn>"
+      icon=""
     fi
     echo "${string}% $icon" > $HOME/.config/xmobar/scripts/volume-pipe
 done
