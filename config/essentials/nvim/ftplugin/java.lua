b6a9e3d97e2096433dc1a840fdc7f0d62bb71c6c
Raymaekers Luca <luca@keyfried.com>
2023-03-05 21:37:23 +0100

    added nvim java specific remaps

--- config/essentials/nvim/ftplugin/java.lua
+++ config/essentials/nvim/ftplugin/java.lua
@@ -1,2 +1,3 @@
-vim.keymap.set({"n", "i"}, "<LocalLeader>t", "<cmd>s/\\(\\s*\\)\\(.*\\)/\\1System.out.println(\"\\2\");<cr>")
+vim.keymap.set({"n", "i"}, "<LocalLeader>t", "<cmd>s/\\(\\s*\\)\\(.*\\)/\\1System.out.print(\"\\2\");<cr>")
 vim.keymap.set({"n", "i"}, "<LocalLeader>i", "<cmd>s/\\(\\s*\\)\\(.*\\)/\\1System.out.print(\"\\2: \" + \\2);<cr>")
+vim.keymap.set({"n", "i"}, "<LocalLeader>l", "<cmd>s/print(/println(/<cr>")
