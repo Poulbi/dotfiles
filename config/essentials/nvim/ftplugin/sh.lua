3c0eb75512f1cecfc2a1fa492d997b10871904e9
Raymaekers Luca <luca@keyfried.com>
2023-04-16 21:28:03 +0200

    added more shell remaps

--- config/essentials/nvim/ftplugin/sh.lua
+++ config/essentials/nvim/ftplugin/sh.lua
@@ -1,3 +1,5 @@
-vim.keymap.set("n", "<LocalLeader>t", "<cmd>s/\\(\\s*\\)\\(.*\\)/\\1echo \"\\2\"<cr><cmd>setlocal nohls<cr>")
-vim.keymap.set("n", "<LocalLeader>i", "<cmd>s/\\(\\s*\\)\\(.*\\)/\\1echo \"\\2: ${\\2}\"<cr><cmd>setlocal nohls<cr>")
+vim.keymap.set("n", "<LocalLeader>t", "<cmd>s/\\(\\s*\\)\\(.*\\)/\\1echo \"\\2\"<cr>")
+vim.keymap.set("n", "<LocalLeader>i", "<cmd>s/\\(\\s*\\)\\(.*\\)/\\1echo \"\\2: $\\2\"<cr>")
+vim.keymap.set("i", "<LocalLeader>t", "<cmd>s/\\(\\s*\\)\\(.*\\)/\\1echo \"\\2\"<cr><esc>A")
+vim.keymap.set("i", "<LocalLeader>i", "<cmd>s/\\(\\s*\\)\\(.*\\)/\\1echo \"\\2: $\\2\"<cr><esc>A")
 vim.opt.formatoptions = "cqrnj"
