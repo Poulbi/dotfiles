1a0d5bf87876f8c397dcc0f7e5cdeecd7a582ca4
Raymaekers Luca <luca@keyfried.com>
2023-08-04 22:45:28 +0200

    [aliases] added ufwd

--- config/essentials/zsh/aliases.sh
+++ config/essentials/zsh/aliases.sh
@@ -40,6 +40,7 @@ fi
 if grep -qi "debian\|ubuntu" /etc/os-release 2> /dev/null
 then
 	alias aptup='apt update && apt upgrade -y'
+	alias ufwd='echo y | ufw delete "$(ufw status numbered | tail -n +5 | fzf | cut -f2 -d'\''['\'' | cut -f1 -d'\'']'\'')"'
 fi
 
 # Programs
