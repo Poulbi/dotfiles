79f090141801bf3e9938119ce2cf38e1394dcdd7
Raymaekers Luca <luca@keyfried.com>
2023-08-30 08:28:55 +0200

    [nvim] moved common lsp from java.lua to setup.lua

--- config/essentials/nvim/ftplugin/java.lua
+++ config/essentials/nvim/ftplugin/java.lua
@@ -12,22 +12,6 @@ end
 local on_attach = function(_, bufnr)
   -- Regular Neovim LSP client keymappings
   local bufopts = { noremap=true, silent=true, buffer=bufnr }
-  nnoremap('gD', vim.lsp.buf.declaration, bufopts, "Go to declaration")
-  nnoremap('gd', vim.lsp.buf.definition, bufopts, "Go to definition")
-  nnoremap('gi', vim.lsp.buf.implementation, bufopts, "Go to implementation")
-  nnoremap('K', vim.lsp.buf.hover, bufopts, "Hover text")
-  nnoremap('<C-k>', vim.lsp.buf.signature_help, bufopts, "Show signature")
-  nnoremap('<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts, "Add workspace folder")
-  nnoremap('<leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts, "Remove workspace folder")
-  nnoremap('<leader>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, bufopts, "List workspace folders")
-  nnoremap('<leader>D', vim.lsp.buf.type_definition, bufopts, "Go to type definition")
-  nnoremap('<leader>rn', vim.lsp.buf.rename, bufopts, "Rename")
-  nnoremap('<leader><Return>', vim.lsp.buf.code_action, bufopts, "Code actions")
-  vim.keymap.set('v', "<leader><Return>", "<ESC><CMD>lua vim.lsp.buf.range_code_action()<CR>",
-    { noremap=true, silent=true, buffer=bufnr, desc = "Code actions" })
-  nnoremap('<leader>f', function() vim.lsp.buf.format { async = true } end, bufopts, "Format file")
 
   -- Java extensions provided by jdtls
   nnoremap("<leader><leader>i", jdtls.organize_imports, bufopts, "Organize imports")
