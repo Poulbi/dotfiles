29b54011c78af23e83d81f73b1167e9c2fe2c968
Raymaekers Luca <luca@keyfried.com>
2023-08-30 08:29:27 +0200

    [nvim] added telescope-media-files

--- config/essentials/nvim/after/plugin/telescope.lua
+++ config/essentials/nvim/after/plugin/telescope.lua
@@ -1,3 +1,6 @@
+require('telescope').load_extension('fzf')
+require('telescope').load_extension('media_files')
+
 require('telescope').setup({
 	defaults = {
 		path_display = {
@@ -14,13 +17,17 @@ require('telescope').setup({
 			override_generic_sorter = true,
 			override_file_sorter = true,
 			case_mode = "smart_case",
+		},
+		media_files = {
+			filetypes = {"png", "webp", "jpg", "jpeg"},
+			find_cmd = "rg"
 		}
 	}
 })
-require('telescope').load_extension('fzf')
 
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files)
+vim.keymap.set('n', '<leader>fb', builtin.buffers)
 vim.keymap.set('n', '<leader>fg', builtin.git_files)
 vim.keymap.set('n', '<leader>fw', builtin.live_grep)
 -- symbols
@@ -35,23 +42,7 @@ require("telescope").setup {
       require("telescope.themes").get_dropdown {
         -- even more opts
       }
-
-      -- pseudo code / specification for writing custom displays, like the one
-      -- for "codeactions"
-      -- specific_opts = {
-      --   [kind] = {
-      --     make_indexed = function(items) -> indexed_items, width,
-      --     make_displayer = function(widths) -> displayer
-      --     make_display = function(displayer) -> function(e)
-      --     make_ordinal = function(e) -> string
-      --   },
-      --   -- for example to disable the custom builtin "codeactions" display
-      --      do the following
-      --   codeactions = false,
-      -- }
     }
   }
 }
--- To get ui-select loaded and working with telescope, you need to call
--- load_extension, somewhere after setup function:
 require("telescope").load_extension("ui-select")
