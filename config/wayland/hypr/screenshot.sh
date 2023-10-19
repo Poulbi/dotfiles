692f420ea79becf470798c889a6bf1548483bf22
Raymaekers Luca <luca@keyfried.com>
2023-10-19 11:24:49 +0200

    refactor

--- config/wayland/hypr/screenshot.sh
+++ config/wayland/hypr/screenshot.sh
@@ -6,23 +6,11 @@ date="$(date +%y%m%d_%H_%M_%S)"
 mkdir -p "$dir"
 
 case $1 in
-	"-m")
-		actwin="$(hyprctl activewindow -j | jq -r '.monitor')"
-		actmon="$(hyprctl monitors -j |
-			jq -r ".[] | select(.id == $actwin)" |
-			jq -r '.name')" 
-		grim -o "$actmon" "$dir/${date}_mon.png"
-		;;
-	"-f")
-		grim "$dir/${date}_full.png"
-		;;
-	"-s")
-		grim -g "$(slurp)" "$dir/${date}_sel.png"
-		;;
-	"-sc")
-		grim -g "$(slurp)" - | wl-copy
-		;;
-	*)
-		exit
-		;;
+	"-m") grim -o "$(hyprctl monitors -j |
+			jq -r ".[] | select(.id == $(hyrctl activewindow -j | jq -r '.monitor'))" |
+			jq -r '.name')" "$dir/${date}_mon.png" ;;
+	"-f") grim "$dir/${date}_full.png" ;;
+	"-s") grim -g "$(slurp)" "$dir/${date}_sel.png" ;;
+	"-sc") grim -g "$(slurp)" - | wl-copy ;;
+	*) exit ;;
 esac
